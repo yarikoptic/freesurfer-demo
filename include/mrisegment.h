cfc72170beb9d138a5f030fb80b01fd0cb8e89d8
Nick Schmansky <nicks@nmr.mgh.harvard.edu>
2010-11-10 18:05:40 +0000

    ENH: MRIsegmentFill now returns MRI pointer instead of int error code

--- include/mrisegment.h
+++ include/mrisegment.h
@@ -7,9 +7,9 @@
 /*
  * Original Author: REPLACE_WITH_FULL_NAME_OF_CREATING_AUTHOR 
  * CVS Revision Info:
- *    $Author: fischl $
- *    $Date: 2007/02/10 01:38:52 $
- *    $Revision: 1.10 $
+ *    $Author: nicks $
+ *    $Date: 2010/11/10 18:05:40 $
+ *    $Revision: 1.10.4.1 $
  *
  * Copyright (C) 2002-2007,
  * The General Hospital Corporation (Boston, MA). 
@@ -71,7 +71,7 @@ int              MRIsegmentFree(MRI_SEGMENTATION **pmriseg) ;
 MRI_SEGMENTATION *MRIsegmentAlloc(int max_segments, int max_voxels) ;
 MRI              *MRIsegmentToImage(MRI *mri_src, MRI *mri_dst,
                                     MRI_SEGMENTATION *mriseg, int s) ;
-int              MRIsegmentFill(MRI_SEGMENTATION *mriseg, int s, MRI *mri, float fillval) ;
+MRI              *MRIsegmentFill(MRI_SEGMENTATION *mriseg, int s, MRI *mri, float fillval) ;
 int              MRIsegmentDilate(MRI_SEGMENTATION *mriseg, MRI *mri) ;
 int              MRIsegmentDilateThreshold(MRI_SEGMENTATION *mriseg,
     MRI *mri_binary, MRI *mri_thresh,
